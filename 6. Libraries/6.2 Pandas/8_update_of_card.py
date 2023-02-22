d8fbb1fcd30b13471317f190ce027973d273ae3d
danilnev <danilanevinsky@gmail.com>
2023-02-22 16:12:29 +0300

    Not full make task 8 in lesson 6.2 (pandas)

---
+++ 6. Libraries/6.2 Pandas/8_update_of_card.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def update(card):
+    result_df = card.copy()
+    result_df['average'] = ((result_df['maths'] if result_df['maths'].any() else 0) +
+                            (result_df['physics'] if result_df['physics'].any() else 0) +
+                            (result_df['computer science'] if result_df['computer science'].any() else 0)) / (
+                               3 if result_df['maths'].any() and result_df['physics'].any() and result_df[
+                                   'computer science'].any()
+                               else (2 if ((result_df['maths'].any() and result_df['physics'].any()) or
+                                           (result_df['maths'].any() and result_df['computer science'].any()) or
+                                           (result_df['physics'].any() and result_df['computer science'].any()))
+                                     else 1)
+                           )
+    return result_df.sort_values('name').sort_values('average', ascending=False)
+
+
+columns = ['name', 'maths', 'physics', 'computer science']
+data = {
+    'name': ['Иванов', 'Петров', 'Сидоров', 'Васечкин', 'Николаев'],
+    'mahs': [5, 4, 5, 2, 4],
+    'pysics': [4, 4, 4, 5, 5],
+    'coputer science': [5, 2, 5, 4, 3]
+}
+journal = pd.DataFrame(data, columns=columns)
+filtered = update(journal)
+print(journal)
+print(filtered)
